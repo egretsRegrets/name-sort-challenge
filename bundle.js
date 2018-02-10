e1f44a257e0fa877dd369ff7f2b5cd434e065766
mercantilism <samuel.galen.gardner@gmail.com>
2018-02-10 11:15:17 -0800

    setup

---
+++ bundle.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var sampleInput = ['Nick', 'jake', 'RAY', 'Kate', 'Nick', 'Jeremy', 'Ryan', 'nick', 'AMOL', 'rAY', 'VIANNEY', 'Samuel', 'ryan', 'Shilpika', 'nick', 'THOMAS', 'tom', 'james', 'JERM', 'amOl', 'kate', 'SCOTT', 'Jenifer', 'bill', 'jenny', 'STEVEN'];
